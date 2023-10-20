60a22005bb30c70d2914b2f601782262f271092c
ikerortegaa <ikerortegabernal@gmail.com>
2023-10-20 11:55:37 +0200

    Añado el primer alert dialog a la actividad, incluyendo Toast.

--- app/src/main/java/com/example/ej1/MainActivity.kt
+++ app/src/main/java/com/example/ej1/MainActivity.kt
@@ -1,20 +1,25 @@
 package com.example.ej1
 
+import android.app.AlertDialog
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.window.Dialog
 import com.example.ej1.ui.theme.Ej1Theme
+import java.lang.StringBuilder
 
 class   MainActivity : ComponentActivity() {
 
@@ -30,13 +35,30 @@ class   MainActivity : ComponentActivity() {
 
 
         button.setOnClickListener {
-            val intent = Intent(this,Saludo::class.java)
+            alerta()
+           /* val intent = Intent(this,Saludo::class.java)
             intent.putExtra("datos",editText.text.toString())
-            startActivity(intent)
+            startActivity(intent)*/
         }
 
+    }
+    fun alerta(){
+        val builder = AlertDialog.Builder(this);
+        builder.setTitle("Primer AlertDialog")
+        builder.setMessage("Primer ejemplo de un alertDialog en android")
+         builder.setPositiveButton(android.R.string.ok){ dialog, which ->
+             Toast.makeText(applicationContext, android.R.string.ok, Toast.LENGTH_SHORT).show()
+             val intent = Intent(this,Saludo::class.java)
+             intent.putExtra("datos",editText.text.toString())
+             startActivity(intent)
+         }
 
+        builder.setNegativeButton(android.R.string.cancel){ dialog, which ->
+            Toast.makeText(applicationContext, android.R.string.cancel, Toast.LENGTH_SHORT).show()}
 
+        builder.setNeutralButton("omitir"){ dialog, which ->
+            Toast.makeText(applicationContext, "omitir", Toast.LENGTH_SHORT).show()}
+        builder.show()
 
 
     }
