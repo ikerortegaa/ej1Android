389e9c8feef81caf82751868d5cfb0a407bf00fa
ikerortegaa <ikerortegabernal@gmail.com>
2023-10-26 14:13:02 +0200

    Evito que el teclado mueva toda la actividad.

--- app/src/main/java/com/example/ej1/MainActivity.kt
+++ app/src/main/java/com/example/ej1/MainActivity.kt
@@ -6,21 +6,9 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.DatePicker
 import android.widget.EditText
-import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.window.Dialog
-import com.example.ej1.ui.theme.Ej1Theme
-import java.lang.StringBuilder
+
 
 class   MainActivity : ComponentActivity() {
 
